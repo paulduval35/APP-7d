c5c98fc3551f3bda5b86da74a4c2e29a9862da1c
HBYann <37926281+HBYann@users.noreply.github.com>
2018-04-16 20:40:46 +0200

    Avancement page d'acceuil
    J'ai essayé d'utiliser flexbox... ça à pas marché !

--- DHOMS/Vues/pageinscripton.php
+++ DHOMS/Vues/pageinscripton.php
@@ -4,38 +4,45 @@
 <head>
     <title>DHOMS</title>
 	<link rel="stylesheet" href="styleinscription.css">
-	
 </head>
 
 <body>
-<?php require "nav.php";?>
-<link rel="stylesheet" href="style.css">
+    <?php require "nav.php";?>
 
 <h1><a href="index.html"><img src="image/accueil/accueil.png" alt="accueil"></a></h1>
 <h1>DHOMS ACCUEIL</h1>
 
 <h2>Bienvenue</h2>
+<div id="acceuil">
 
-<form action="somescript.php" method="post">
+    <div class="element">
+        <h3> Connexion </h3>
+        <form action="connexion.php" method="post">
+            <p><input type="text" value="Adresse mail"></p>
+          <p><input password="password" value="Mot de passe"></p>
 
-<p>e-mail:</p>
-<p><input type="text" value="email"></p>
-<p>mot de passe</p>
+            <p>tu es :</p>
+         <p><input type="radio" name="areyou" value="user"> utilisateur</p>
+         <p><input type="radio" name="areyou" value="admin"> administrateur</p>
 
-<p><input password="password" value="ton mot de passe"></p>
+          <p><input type="submit" value="Connexion"></p>
+        </form>
+    </div>
 
+    <div class="element">
+        <h3> Inscription </h3>
+        <form action="inscription.php" method="post">
+            <p><input type="text" value="Adresse mail"></p>
+            <p><input password="password" value="Mot de passe"></p>
+            <p><input password="password" value="Confirmation mot de passe"></p>
+            <p><input type="text" value="Code client"></p>
 
-<p>tu es:</p>
-<p><input type="radio" name="areyou" value="user"> utilisateur</p>
-<p><input type="radio" name="areyou" value="admin"> administrateur</p>
+            <p><input type="radio" name="CGU" value="CGU"> J'ai lu et j'accepte les CGU</p>
 
-
-<p><input type="submit"></p>
-
-
-
-</form>
-
+            <p><input type="submit" value="S'inscrire"></p>
+        </form>
+    </div>
+</div>
 
 </body>
 
