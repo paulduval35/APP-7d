8c8732e58fba90def06fe05f8c1abbd24be46748
HBYann <37926281+HBYann@users.noreply.github.com>
2018-05-02 10:25:10 +0200

    database link (personne)

--- DHOMS/Modele/connect_database.php
+++ DHOMS/Modele/connect_database.php
@@ -1,23 +1,19 @@
 <?php
 
-// TEST PREMIERE METHODE DE CONNEXION A LA BDD
+// CONNEXION A LA BDD
 
-//   $dbname = "dhoms";
-//   $host='localhost';
-//   $user='root';
-//   $pass=
+   $dbname = "dhoms";
+   $host='localhost';
+   $user='root';
+   $pass=
 
-//   try {
-//       $db = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$pass");
-//       $db->query("SET NAMES UTF8");
-//       $_SERVER['name'] = $db;
+   try {
+       $db = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$pass");
+       $db->query("SET NAMES UTF8");
+       $_SERVER['name'] = $db;
 
-//   } catch (Exception $e) {
-//       die('Erreur: '.$e->getMessage());
-//   }
-
-// TEST DEUXIEME METHODE DE CONNEXION A LA BDD
-
-$bdd = new PDO('mysql:host=localhost:3306;dbname=dhoms;charset=utf8', 'root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+   } catch (Exception $e) {
+       die('Erreur: '.$e->getMessage());
+   }
 
 ?>
