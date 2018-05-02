c00ab64f36b65562b69b59db6e95be3b73cc3476
Ek-Andreas <38030748+Ek-Andreas@users.noreply.github.com>
2018-05-02 11:27:14 +0200

    modification accueil

--- DHOMS/Vue/accueil.php
+++ DHOMS/Vue/accueil.php
@@ -16,24 +16,20 @@
             <h3 id="connexion_inscription"> Connexion </h3>
 
             <form action="connexion.php" method="post" id="p_connexion">
-                <p><input type="text" value="Adresse mail"></p> <!-- ajouter système pour filtrer les caractères non supportés par e-mail -->
-                <p><input password="password" value="Mot de passe"></p> <!-- mettre un système -->
-                <p>tu es :</p>
-                <p><input type="radio" name="areyou" value="user"> utilisateur</p>
-                <p><input type="radio" name="areyou" value="admin"> administrateur</p>
+                <p>  <input id="mail" type="email" name="email" placeholder=" E-mail" required title="E-mail du type : user@domain.com"/> </p>
+				<p> <input type="password" class=MotDePasse  name="password" required placeholder="Mot de passe"> </p>
                 <p><input type="submit" value="Connexion"></p>
             </form>
         </div>
 
     <div class="inscription">
             <h3 id="connexion_inscription"> Inscription </h3>
-            <form action="inscription.php" method="post" id="p_inscription">
-                <p><input type="text" value="Adresse mail"></p>
-                <p><input password="password" name=password value="Mot de passe"></p>
-                <p><input password="password" name= confpassword value="Confirmation mot de passe"></p>
-                <p><input type="text" value="Code client"></p>
-                <p><input type="radio" name="CGU" value="CGU"> J'ai lu et j'accepte les CGU</p>
-                <p><input type="submit" value="S'inscrire"></p>
+            <form action="inscription.php" method="post">
+			<p><input id="mailins" type="email" name="email" placeholder=" E-mail" required title="E-mail: user@domain.com"/></p>
+			<p>	<input id="mdpm" type="password" name="mdp" placeholder="Mot de passe" title="Minimum 8 caractères (uniquement des chiffres et des lettres);" required/></p>
+			<p><input id="mdp2" type="password" name="mdp2" placeholder="Confirmer le mot de passe" required></p>
+            <p><input type="checkbox" name="CGU" value="CGU"> J'ai lu et j'accepte les CGU</p>
+            <p><input type="submit" value="S'inscrire"></p>
             </form>
     </div>
     </div>
