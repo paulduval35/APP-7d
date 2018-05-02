991d2d6e97d172c984b8e6f2e2e7226a302a56d9
PaulHerisson <38030038+PaulHerisson@users.noreply.github.com>
2018-05-02 11:08:45 +0200

    chgmt id pour désigner les <p> et modif couleur

--- DHOMS/Vue/accueil.php
+++ DHOMS/Vue/accueil.php
@@ -13,10 +13,10 @@
     <div id="main_accueil">
         <div class="box_gauche_main_accueil"></div>
         <div class="connexion">
-            <h3> Connexion </h3>
+            <h3 id="connexion_inscription"> Connexion </h3>
 
-            <form action="connexion.php" method="post">
-                <p><input type="text" value="Adresse mail"></p> <!-- ajouter système pour filtrer les characteres non supportés par e-mail -->
+            <form action="connexion.php" method="post" id="p_connexion">
+                <p><input type="text" value="Adresse mail"></p> <!-- ajouter système pour filtrer les caractères non supportés par e-mail -->
                 <p><input password="password" value="Mot de passe"></p> <!-- mettre un système -->
                 <p>tu es :</p>
                 <p><input type="radio" name="areyou" value="user"> utilisateur</p>
@@ -26,8 +26,8 @@
         </div>
 
     <div class="inscription">
-            <h3> Inscription </h3>
-            <form action="inscription.php" method="post">
+            <h3 id="connexion_inscription"> Inscription </h3>
+            <form action="inscription.php" method="post" id="p_inscription">
                 <p><input type="text" value="Adresse mail"></p>
                 <p><input password="password" name=password value="Mot de passe"></p>
                 <p><input password="password" name= confpassword value="Confirmation mot de passe"></p>
