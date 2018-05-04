1ef335ca464a7fe4158595221f23449c613afbdd
pidakichi <32439606+pidakichi@users.noreply.github.com>
2018-05-04 19:56:29 +0200

    vue page inscription presque finie

--- DHOMS/Vue/inscription_vue.php
+++ DHOMS/Vue/inscription_vue.php
@@ -8,7 +8,7 @@
 
 <body>
 
-    <div id="squelette">
+
         <nav>
             <?php include "nav.php"?>
         </nav>
@@ -18,14 +18,103 @@
                 <?php include "header.php"?>
             </header>
 
+            <section>
+                <h3 id="connexion_inscription"> INSCRIPTION </h3><br>
+
+
+
+                <form id ="form_inscription" action="../Controller/inscription_controller.php" method="post" id="p_inscription">
+
+                    <ul class = "flex-outer">
+                        <li>
+                            <label>
+                                Nom
+                            </label>
+
+                            <input id = "nom_inscription" type = "text" name = "nom" required>
+                        </li>
+
+                        <li>
+                            <label>
+                                Prénom
+                            </label>
+
+                            <input id = "prenom_inscription" type = "text" name = "prenom" required>
+                        </li>
+
+                        <li>
+                            <label>
+                                E-mail
+                            </label>
+                            <input id="mail_inscription" type="email" name="email"  required title="E-mail: user@domain.com"/>
+                        </li>
+
+                        <li>
+                            <label>
+                                Mot de passe
+                            </label>
+
+
+                            <input id="mdpm" type="password" name="mdp"  title="Minimum 8 caractères (uniquement des chiffres et des lettres);" required/>
+                        </li>
+
+                        <li>
+                            <label>
+                                Confirmer le mot de passe
+                            </label>
+
+                            <input id="mdp2" type="password" name="mdp2"  required>
+                        </li>
+
+                        <li>
+                            <label>
+                                Statut
+                            </label>
+
+                            <select id = "statut_inscription" name = "statut" required>
+                                <option>opt1</option>
+                                <option>opt2</option>
+                            </select>
+                        </li>
+
+                        <li>
+                            <label>
+                                Numéro de téléphone fixe
+                            </label>
+
+                            <input id = "numfixe_inscription" name = "numfixe" required>
+                        </li>
+
+                        <li>
+                            <label>
+                                Numéro de téléphone mobile
+                            </label>
+
+                            <input id = "nummobile_inscription" name = "nummobile" required>
+                        </li>
+
+                        <li>
+                            <input id = "checkbox" type="checkbox" name="CGU" value="CGU">J'ai lu et j'accepte les CGU
+                        </li>
+
+                        <li>
+                            <button type="submit">S'inscrire</button>
+                        </li>
+
+
+                    </ul>
+                </form>
+
+
+
+            </section>
+
+
             <footer>
                 <?php include "footer.php"?>
             </footer>
 
-            <section>
-                METTRE CE QUE VOUS VOULEZ ICI
-            </section>
         </div>
-    </div>
+
 
 </body>
