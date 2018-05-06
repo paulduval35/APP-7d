e01257e0ae8f8b04087d2985c6c126aab91f509a
PaulHerisson <38030038+PaulHerisson@users.noreply.github.com>
2018-05-07 00:07:21 +0200

    nav fonctionnel

--- DHOMS/Vue/nav.php
+++ DHOMS/Vue/nav.php
@@ -6,10 +6,10 @@
     <title>DHOMS</title>
 </head>
 <body>
-        <a href="index.html"><img src="image/accueil/accueil.png" alt="accueil"></a>
-        <a href="nosproduits.html"><img src="image/accueil/nosproduits.png" alt="nosproduits"> </a>
-        <a href="espaceclient.html"><img src="image/accueil/espaceclient.png" alt="espaceclient"> </a>
-        <a href="faq.html"><img src="image/accueil/faq.png" alt="faq"> </a>
-        <a href="contact.html"><img src="image/accueil/contact.png" alt="contact"> </a>
+        <a href="inscription_vue.php"><img src="image/accueil/accueil.png" alt="accueil"></a>
+        <a href="nos_produits.php"><img src="image/accueil/nosproduits.png" alt="nosproduits"> </a>
+        <a href="espace_client.php"><img src="image/accueil/espaceclient.png" alt="espaceclient"> </a>
+        <a href="faq.php"><img src="image/accueil/faq.png" alt="faq"> </a>
+        <a href="contact.php"><img src="image/accueil/contact.png" alt="contact"> </a>
 
 </body>
