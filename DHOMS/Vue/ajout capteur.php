1c2fc3998a574c72547d2bb025fe126e3d89e3ca
Ek-Andreas <38030748+Ek-Andreas@users.noreply.github.com>
2018-05-07 12:02:47 +0200

    pas encore fini cette page

--- DHOMS/Vue/ajout capteur.php
+++ DHOMS/Vue/ajout capteur.php
@@ -9,14 +9,35 @@
 </head>
 
 <body> 
+                    <ul class = "flex-outer">
+
 	<?php require "nav.php";?>
 	<p> selectionner le type de capteur</p>
 	 
-	<p> 
+	<p>
+
+  <tr>
+    <td>
+	<a href="ajouter_thermo.php">
+  <img src="thermo.png" alt="thermometre" style="width:42px;height:42px;border:0;">
+	</a></td>
+    <td>
+	<a href="ajouter_thermo.php">
+  <img src="thermo.png" alt="thermometre" style="width:42px;height:42px;border:0;">
+	</a></td> 
+    <td>
+	<a href="ajouter_thermo.php">
+  <img src="thermo.png" alt="thermometre" style="width:42px;height:42px;border:0;">
+	</a></td>
+  </tr>	
+	<a href="ajouter_thermo.php">
+  <img src="thermo.png" alt="thermometre" style="width:42px;height:42px;border:0;">
+	</a>
+	</p>
+	 </ul>
 	 
 	 
-	 
-	 
+	 </body>
 	 <!--<form action="ajout_maison.php" method="post">
 		<p>
 		<select>  
