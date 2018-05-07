c57534613d1884d56fb87753cf6822e7fd9ee098
Ek-Andreas <38030748+Ek-Andreas@users.noreply.github.com>
2018-05-07 11:10:17 +0200

    test

---
+++ DHOMS/Vue/ajout capteur.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>DHOMS</title>
+    <meta charset="UTF-8">
+    <link rel= "stylesheet" href="style.css" />
+	<!-- commentaire -->
+</head>
+
+<body> 
+	<?php require "nav.php";?>
+	<p> selectionner le type de capteur</p>
+	 
+	<p> 
+	 
+	 
+	 
+	 
+	 <!--<form action="ajout_maison.php" method="post">
+		<p>
+		<select>  
+			<option value="température">température</option>
+			<option value="sonore">sonore</option>
+			  <option value="light">lumière</option>
+			  <option value="volets">volets</option>
+			</select>		
+		</p>
+		
+			<p><input type="submit" value="continuer"></p>
+            </form>
+			</body>-->
+			
