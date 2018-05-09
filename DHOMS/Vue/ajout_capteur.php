681653d4a7fded71296f91bdb4640f988ce3700d
PaulHerisson <38030038+PaulHerisson@users.noreply.github.com>
2018-05-09 20:02:58 +0200

    new ajout_capteur

--- DHOMS/Vue/ajout_capteur.php
+++ DHOMS/Vue/ajout_capteur.php
@@ -17,123 +17,149 @@
         <div id="header_footer">
             <header>
                 <?php include "header.php"?>
-            </header>          
+            </header>
+            <section>
+                <div id="ajout_capteurs">
+
 					<ul class = "flex-outer">
 
 	<h2> selectionner le capteur:</h2>
 	 
 	<p>
 <?$selection = $_POST['taskOption'];  ?>
-<table style="width:100%">
   
-<form id ="nouveau_capteur" action="../Controleur/capteur_controlleur.php" method="post" >
-  
-  
-  <tr>
-    <th>thermometre</th>
-    <th>lumière</th> 
-    <th>stores</th>
-	<th>sonore</th>
+<form id ="form_nouv_capteur" action="../Controleur/capteur_controlleur.php" method="post" >
 
-  </tr>
-  <tr>
-    <th><div class="dropdown">
-	<img src="image/capteurs/thermometer.png" alt="whatever"  >
-	<div class="dropdown-content">
-	<button id=val class=butto name="subject" type="submit" value="1">modele1</button>
-    <button id=val class=butto name="subject" type="submit" value="2">modele2</button>
-	<button id=val class=butto name="subject" type="submit" value="3">modele3</button>
+                            <div id ="nouveau_capteur">
+                                <div class="nom_capteur">thermometre</div>
+                                <div class="nom_capteur">lumière</div>
+                                <div class="nom_capteur">stores</div>
+                                <div class="nom_capteur">sonore</div>
+                            </div>
 
-	</div>
-</div></th>
-    
-  
-    <th>
-	<div class="dropdown">
-<img src="image/accueil/accueil.png" alt="whatever"  >
-<div class="dropdown-content">
-    <a href="#">modele 1.1 </a>
-    <a href="#">modele 1.2 </a>
-    <a href="#">modele 1.3 </a>
-  </div>
-</div>
-			</a></th>
-    
- 
-    <th><a href="ajouter_stores.php">
-		<div class="dropdown">
-<img src="image/accueil/accueil.png" alt="whatever"  >
-<div class="dropdown-content">
-    <a href="#">modele 1 1</a>
-    <a href="#">modele 1 2</a>
-    <a href="#">modele 1 3</a>
-  </div>
-</div>
-			</th>
-	<th><div class="dropdown">
-<img src="image/accueil/accueil.png" alt="whatever"  >
-<div class="dropdown-content">
-    <a href="#">modele 1 1</a>
-    <a href="#">modele 1 2</a>
-    <a href="#">modele 1 3</a>
-  </div>
-</div></th>
-	<tr>   </tr>		
-			
-	<tr>
-    <th>machin</th>
-    <th>bidule</th> 
-    <th>truc</th>
-	<th>chouette</th>
+                            <div id = "icone_capteur" >
+                                <div class="selecteur"><img class="icone_capteur" src="image/capteurs/icone_thermometre.png" alt="icone d'un thermomètre"  >
+                                    <div class="selecteur-content">
+                                        <button id=val class=butto name="subject" type="submit" value="1">modele1</button>
+                                        <button id=val class=butto name="subject" type="submit" value="2">modele2</button>
+                                        <button id=val class=butto name="subject" type="submit" value="3">modele3</button>
+                                    </div>
+                                </div>
+                                <div class="selecteur"><img class="icone_capteur" src="image/capteurs/icone_thermometre.png" alt="icone d'un thermomètre"  >
+                                    <div class="selecteur-content">
+                                        <button id=val class=butto name="subject" type="submit" value="1">modele1</button>
+                                        <button id=val class=butto name="subject" type="submit" value="2">modele2</button>
+                                        <button id=val class=butto name="subject" type="submit" value="3">modele3</button>
+                                    </div>
+                                </div>
+                                <div class="selecteur"><img class="icone_capteur" src="image/capteurs/icone_thermometre.png" alt="icone d'un thermomètre"  >
+                                    <div class="selecteur-content">
+                                        <button id=val class=butto name="subject" type="submit" value="1">modele1</button>
+                                        <button id=val class=butto name="subject" type="submit" value="2">modele2</button>
+                                        <button id=val class=butto name="subject" type="submit" value="3">modele3</button>
+                                    </div>
+                                </div>
+                                <div class="selecteur"><img class="icone_capteur" src="image/capteurs/icone_thermometre.png" alt="icone d'un thermomètre"  >
+                                    <div class="selecteur-content">
+                                        <button id=val class=butto name="subject" type="submit" value="1">modele1</button>
+                                        <button id=val class=butto name="subject" type="submit" value="2">modele2</button>
+                                        <button id=val class=butto name="subject" type="submit" value="3">modele3</button>
+                                    </div>
+                                </div>
 
-  </tr>
-  <tr>
-  </tr>
-  <tr>
-    <th><div class="dropdown">
-<img src="image/accueil/accueil.png" alt="whatever"  >
-<div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-</div></th>
-    
-  
-    <th><div class="dropdown">
-<img src="image/accueil/accueil.png" alt="whatever"  >
-<div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-</div></th>
-    
- 
-    <th><div class="dropdown">
-<img src="image/accueil/accueil.png" alt="whatever"  >
-<div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-</div></th>
-			
-	<th><div class="dropdown">
-<img src="image/accueil/accueil.png" alt="whatever"  >
-<div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-</div></th>
+
+                            </div>
+                            <div id ="nouveau_capteur">
+                                <div class="nom_capteur">thermometre</div>
+                                <div class="nom_capteur">lumière</div>
+                                <div class="nom_capteur">stores</div>
+                                <div class="nom_capteur">sonore</div>
+                            </div>
+
+                            <div id = "icone_capteur" >
+                                <div class="selecteur"><img class="icone_capteur" src="image/capteurs/icone_thermometre.png" alt="icone d'un thermomètre"  >
+                                    <div class="selecteur-content">
+                                        <button id=val class=butto name="subject" type="submit" value="1">modele1</button>
+                                        <button id=val class=butto name="subject" type="submit" value="2">modele2</button>
+                                        <button id=val class=butto name="subject" type="submit" value="3">modele3</button>
+                                    </div>
+                                </div>
+                                <div class="selecteur"><img class="icone_capteur" src="image/capteurs/icone_thermometre.png" alt="icone d'un thermomètre"  >
+                                    <div class="selecteur-content">
+                                        <button id=val class=butto name="subject" type="submit" value="1">modele1</button>
+                                        <button id=val class=butto name="subject" type="submit" value="2">modele2</button>
+                                        <button id=val class=butto name="subject" type="submit" value="3">modele3</button>
+                                    </div>
+                                </div>
+                                <div class="selecteur"><img class="icone_capteur" src="image/capteurs/icone_thermometre.png" alt="icone d'un thermomètre"  >
+                                    <div class="selecteur-content">
+                                        <button id=val class=butto name="subject" type="submit" value="1">modele1</button>
+                                        <button id=val class=butto name="subject" type="submit" value="2">modele2</button>
+                                        <button id=val class=butto name="subject" type="submit" value="3">modele3</button>
+                                    </div>
+                                </div>
+                                <div class="selecteur"><img class="icone_capteur" src="image/capteurs/icone_thermometre.png" alt="icone d'un thermomètre"  >
+                                    <div class="selecteur-content">
+                                        <button id=val class=butto name="subject" type="submit" value="1">modele1</button>
+                                        <button id=val class=butto name="subject" type="submit" value="2">modele2</button>
+                                        <button id=val class=butto name="subject" type="submit" value="3">modele3</button>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            <div id ="nouveau_capteur">
+                                <div class="nom_capteur">thermometre</div>
+                                <div class="nom_capteur">lumière</div>
+                                <div class="nom_capteur">stores</div>
+                                <div class="nom_capteur">sonore</div>
+                            </div>
+
+                            <div id = "icone_capteur" >
+                                <div class="selecteur"><img class="icone_capteur" src="image/capteurs/icone_thermometre.png" alt="icone d'un thermomètre"  >
+                                    <div class="selecteur-content">
+                                        <button id=val class=butto name="subject" type="submit" value="1">modele1</button>
+                                        <button id=val class=butto name="subject" type="submit" value="2">modele2</button>
+                                        <button id=val class=butto name="subject" type="submit" value="3">modele3</button>
+                                    </div>
+                                </div>
+                                <div class="selecteur"><img class="icone_capteur" src="image/capteurs/icone_thermometre.png" alt="icone d'un thermomètre"  >
+                                    <div class="selecteur-content">
+                                        <button id=val class=butto name="subject" type="submit" value="1">modele1</button>
+                                        <button id=val class=butto name="subject" type="submit" value="2">modele2</button>
+                                        <button id=val class=butto name="subject" type="submit" value="3">modele3</button>
+                                    </div>
+                                </div>
+                                <div class="selecteur"><img class="icone_capteur" src="image/capteurs/icone_thermometre.png" alt="icone d'un thermomètre"  >
+                                    <div class="selecteur-content">
+                                        <button id=val class=butto name="subject" type="submit" value="1">modele1</button>
+                                        <button id=val class=butto name="subject" type="submit" value="2">modele2</button>
+                                        <button id=val class=butto name="subject" type="submit" value="3">modele3</button>
+                                    </div>
+                                </div>
+                                <div class="selecteur"><img class="icone_capteur" src="image/capteurs/icone_thermometre.png" alt="icone d'un thermomètre"  >
+                                    <div class="selecteur-content">
+                                        <button id=val class=butto name="subject" type="submit" value="1">modele1</button>
+                                        <button id=val class=butto name="subject" type="submit" value="2">modele2</button>
+                                        <button id=val class=butto name="subject" type="submit" value="3">modele3</button>
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+
     
 
-	</p>
-	
-	 </form>
-	</table> 
-	<p>les icons proviennent de Freepik  (www.flaticon.com) </p>
+
+    </form>
+                </div>
+
 		 </ul>
-
+            </section>
+            <footer>
+                <?php include "footer.php"?>
+            </footer>
+        </div>
 	 </body>
+</html>
