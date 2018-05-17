58cc18f8b86dc197e98bd2e28d315da4bdeeb53d
Ek-Andreas <38030748+Ek-Andreas@users.noreply.github.com>
2018-05-18 00:42:29 +0200

    réparé mon truc

--- DHOMS/Modele/gestion_utilisateurs_modele.php
+++ DHOMS/Modele/gestion_utilisateurs_modele.php
@@ -1,11 +1,17 @@
 <?php
-
 include "connect_database_modele.php";
 
-function getUtilisateur(){
-    global $bdd;
-    $reponse = $bdd->query("SELECT * FROM personne ");
-    return $reponse;
-}
+
+function getUtilisateur($email){
+   global $bdd;
+   
+   $query = $bdd->prepare("SELECT `personne`.`prenom` FROM `personne` WHERE `personne`.`email` = '$email'");
+   
+$query->execute();
+
+$row = $query->fetch();
+
+echo $row['prenom'];
+	
 
 ?>
