b02b7ba5382b5e966d31fbbf5bf8a74d8438d53d
Ek-Andreas <38030748+Ek-Andreas@users.noreply.github.com>
2018-06-13 10:39:30 +0200

    ajouter piece

---
+++ DHOMS/Controleur/ajouter_piece_controleur.php
@@ -0,0 +1,22 @@
+
+<?php
+
+$nom=$_POST["nom"]; 
+$surface=$_POST["surface"];
+$maison=$_POST["IDmaison"];
+
+include "../Modele/ajouter_piece_modele.php";
+if( insertPiece($nom,$surface,$maison)){
+?> 
+<script language="javascript">
+		alert( "la piece a été ajouté")
+ window.location.href = "../Controleur/tableau_bord_controleur.php";</script>
+<?php
+exit();
+}else{
+alert( "erreur")
+ window.location.href = "../Controleur/tableau_bord_controleur.php";</script>	
+	?>
+<?php
+}
+?>
---
+++ DHOMS/Vue/ajouter_piece_vue.php
@@ -0,0 +1,63 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>DHOMS</title>
+    <meta charset="UTF-8">
+
+    <link rel="stylesheet" href="../Vue/style.css">
+	<!-- commentaire -->
+</head>
+
+<body>
+    <?php include "../Controleur/url.php"?>
+	
+    <nav>
+        <?php include "nav.php"?>
+    </nav>
+
+    <div id = "header_footer">
+        <header>
+            <?php include "header.php"?>
+        </header>
+<?//$IDmaison = $_POST["IDmaison"];?>
+		<section>
+			<form id ="ajouter_salle" action="../Controleur/ajouter_piece_controleur.php" method="post">
+            <ul class = "flex-outer">
+
+				<li>
+                    <label for="nom_salle">
+                        Nom de la salle
+                    </label>
+                    <input id="nom_piece" type="text" name="nom"  required  placeholder="ENTREZ LE NOM DE LA SALLE">
+                </li>
+				<li>
+                    <label for="surface">
+                        Surface
+                    </label>
+                    <input id="surface" type="number" name="surface"  required  placeholder="LA SURFACE DE LA SALLE">
+                </li>
+				<input id="IDmaison" name="IDmaison" type="hidden" value="<?php echo "$IDmaison"?> ">
+
+				<li>
+                    <button type="submit" name="bouton">Ajouter la salle</button>
+                </li>
+				
+			</form>
+		 </section>
+        <footer>
+            <?php include "footer.php"?>
+        </footer>
+    </div>
+
+
+</body>
+</html>
+		
+		
+		
+		
+		
+		
